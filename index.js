ee06bc3704b6a661046bf5b7f7d831121168c868
Denys Korotkoruchko <denenergii@gmail.com>
2022-10-21 13:24:40 +0300

    ready

--- index.js
+++ index.js
@@ -1,152 +1,156 @@
 const canvas = document.getElementById("canvas");
 const buttonClear = document.getElementById("clear");
 const ctx = canvas.getContext("2d");
-const storedLines = [];
-let startX = 0;
-let startY = 0;
-let isStartDraw = false;
 
 canvas.width = 500;
 canvas.height = 500;
-
 ctx.strokeStyle = "blue";
 ctx.lineWidth = 2;
 
-canvas.addEventListener('click', event => {
-  isStartDraw ? handleFinishDraw(event) : handleStartDraw(event)
-});
+class Paint {
+  #startX = 0;
+  #startY = 0;
+  #storedLines = [];
+  #requestId;
+  #isStartDraw = false;
 
-canvas.addEventListener('mousemove', event => handleMouseMove(event));
-
-buttonClear.addEventListener('click', () => {
-  redrawStoredLines();
-  deleteAnimation();
-});
-
-function handleStartDraw(e) {
-  const mouseX = parseInt(e.clientX);
-  const mouseY = parseInt(e.clientY);
-
-  isStartDraw = true;
-  startX = mouseX;
-  startY = mouseY;
-
-}
-
-function handleMouseMove(e) {
-
-  if (!isStartDraw) {
-    return;
-  }
-
-  redrawStoredLines();
-
-  const mouseX = parseInt(e.clientX);
-  const mouseY = parseInt(e.clientY);
-
-  // draw the current line
-  ctx.beginPath();
-  ctx.moveTo(startX, startY);
-  ctx.lineTo(mouseX, mouseY);
-  ctx.stroke()
-
-  const storedLinesNew = {
-    x1: startX,
-    y1: startY,
-    x2: mouseX,
-    y2: mouseY
+  constructor() {
   };
 
-  if (storedLines.length > 0) {
-    for (let i = 0; i < storedLines.length; i++) {
-      intersectionPoint(storedLines[i], storedLinesNew);
-    }
-  }
-}
+  getIsStartDraw() {
+    return this.#isStartDraw;
+  };
 
+  redrawStoredLines() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-function handleFinishDraw(e) {
+    if (this.#storedLines.length === 0) {
+      return;
+    };
 
-  isStartDraw = false;
+    for (let i = 0; i < this.#storedLines.length; i++) {
+      ctx.beginPath();
+      ctx.moveTo(this.#storedLines[i].x1, this.#storedLines[i].y1);
+      ctx.lineTo(this.#storedLines[i].x2, this.#storedLines[i].y2);
+      ctx.stroke();
+    };
+  };
 
-  const mouseX = parseInt(e.clientX);
-  const mouseY = parseInt(e.clientY);
+  handleStartDraw(e) {
+    const mouseX = parseInt(e.clientX);
+    const mouseY = parseInt(e.clientY);
 
-  storedLines.push({
-    x1: startX,
-    y1: startY,
-    x2: mouseX,
-    y2: mouseY
-  });
+    this.#isStartDraw = true;
+    this.#startX = mouseX;
+    this.#startY = mouseY;
+  };
 
-  redrawStoredLines();
-  if (storedLines.length > 1) {
-    for (let i = 0; i < storedLines.length; i++) {
-      for (let j = i; j < storedLines.length; j++)
-        intersectionPoint(storedLines[i], storedLines[j]);
-    }
-  }
-}
+  handleMouseMove(e) {
+    if (!this.#isStartDraw) {
+      return;
+    };
 
+    this.redrawStoredLines();
 
-function redrawStoredLines() {
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  if (storedLines.length === 0) {
-    return;
-  }
-
-  for (let i = 0; i < storedLines.length; i++) {
-    ctx.beginPath();
-    ctx.moveTo(storedLines[i].x1, storedLines[i].y1);
-    ctx.lineTo(storedLines[i].x2, storedLines[i].y2);
-    ctx.stroke();
-  }
-}
-
-
-let requestId; // для управления AnimationFrame
-
-function deleteAnimation() {
-  setTimeout(() => {
-    storedLines.length = 0;
-    window.cancelAnimationFrame(requestId);
-    requestId = undefined;
-    redrawStoredLines();
-  }, 3000);
-    requestId = window.requestAnimationFrame(deleteAnimation);
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < storedLines.length; i++) {
-    let newX1;
-    let newY1;
-    let newX2;
-    let newY2;
-
-    if (storedLines[i].x1 > storedLines[i].x2) {
-      newX1 = storedLines[i].x1 -= (storedLines[i].x1 - storedLines[i].x2) / (canvas.width / 10);
-      newX2 = storedLines[i].x2 += (storedLines[i].x1 - storedLines[i].x2) / (canvas.width / 10);
-    } else {
-      newX1 = storedLines[i].x1 += (storedLines[i].x2 - storedLines[i].x1) / (canvas.width / 10);
-      newX2 = storedLines[i].x2 -= (storedLines[i].x2 - storedLines[i].x1) / (canvas.width / 10);
-    }
-
-    if (storedLines[i].y1 > storedLines[i].y2) {
-      newY1 = storedLines[i].y1 -= (storedLines[i].y1 - storedLines[i].y2) / (canvas.height / 10);
-      newY2 = storedLines[i].y2 += (storedLines[i].y1 - storedLines[i].y2) / (canvas.height / 10);
-    } else {
-      newY1 = storedLines[i].y1 += (storedLines[i].y2 - storedLines[i].y1) / (canvas.height / 10);
-      newY2 = storedLines[i].y2 -= (storedLines[i].y2 - storedLines[i].y1) / (canvas.height / 10);
-    }
+    const mouseX = parseInt(e.clientX);
+    const mouseY = parseInt(e.clientY);
 
     ctx.beginPath();
-    ctx.moveTo(storedLines[i].x1, storedLines[i].y1);
-    ctx.lineTo(storedLines[i].x2, storedLines[i].y2);
+    ctx.moveTo(this.#startX, this.#startY);
+    ctx.lineTo(mouseX, mouseY);
     ctx.stroke();
-    if (storedLines.length > 1) {
-      for (let j = i; j < storedLines.length; j++)
-        intersectionPoint(storedLines[i], storedLines[j]);
-    }
+
+    const storedLinesNew = {
+      x1: this.#startX,
+      y1: this.#startY,
+      x2: mouseX,
+      y2: mouseY
+    };
+
+    if (this.#storedLines.length > 0) {
+      for (let i = 0; i < this.#storedLines.length; i++) {
+        intersection.intersectionPoint(this.#storedLines[i], storedLinesNew);
+      };
+    };
+  };
+
+  handleFinishDraw(e) {
+    this.#isStartDraw = false;
+
+    const mouseX = parseInt(e.clientX);
+    const mouseY = parseInt(e.clientY);
+
+    const newLine = {
+      x1: this.#startX,
+      y1: this.#startY,
+      x2: mouseX,
+      y2: mouseY
+    };
+
+    this.#storedLines.push(newLine);
+
+    this.redrawStoredLines();
+    if (this.#storedLines.length > 1) {
+      for (let i = 0; i < this.#storedLines.length; i++) {
+        for (let j = i; j < this.#storedLines.length; j++)
+        intersection.intersectionPoint(this.#storedLines[i], this.#storedLines[j]);
+      };
+    };
+  };
+
+  deleteAnimation = () => {
+    setTimeout(() => {
+      this.#storedLines.length = 0;
+      window.cancelAnimationFrame(this.#requestId);
+      this.#requestId = undefined;
+      this.redrawStoredLines();
+    }, 3000);
+    this.#requestId = window.requestAnimationFrame(this.deleteAnimation);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < this.#storedLines.length; i++) {
+
+      if (this.#storedLines[i].x1 > this.#storedLines[i].x2) {
+        this.#storedLines[i].x1 -= (this.#storedLines[i].x1 - this.#storedLines[i].x2) / (canvas.width / 10);
+        this.#storedLines[i].x2 += (this.#storedLines[i].x1 - this.#storedLines[i].x2) / (canvas.width / 10);
+      } else {
+        this.#storedLines[i].x1 += (this.#storedLines[i].x2 - this.#storedLines[i].x1) / (canvas.width / 10);
+        this.#storedLines[i].x2 -= (this.#storedLines[i].x2 - this.#storedLines[i].x1) / (canvas.width / 10);
+      };
+
+      if (this.#storedLines[i].y1 > this.#storedLines[i].y2) {
+        this.#storedLines[i].y1 -= (this.#storedLines[i].y1 - this.#storedLines[i].y2) / (canvas.height / 10);
+        this.#storedLines[i].y2 += (this.#storedLines[i].y1 - this.#storedLines[i].y2) / (canvas.height / 10);
+      } else {
+        this.#storedLines[i].y1 += (this.#storedLines[i].y2 - this.#storedLines[i].y1) / (canvas.height / 10);
+        this.#storedLines[i].y2 -= (this.#storedLines[i].y2 - this.#storedLines[i].y1) / (canvas.height / 10);
+      };
+
+      ctx.beginPath();
+      ctx.moveTo(this.#storedLines[i].x1, this.#storedLines[i].y1);
+      ctx.lineTo(this.#storedLines[i].x2, this.#storedLines[i].y2);
+      ctx.stroke();
+      if (this.#storedLines.length > 1) {
+        for (let j = i; j < this.#storedLines.length; j++)
+        intersection.intersectionPoint(this.#storedLines[i], this.#storedLines[j]);
+      };
+    };
+  };
+};
+
+const painting = new Paint();
+
+canvas.addEventListener('click', event => {
+  painting.getIsStartDraw() ? painting.handleFinishDraw(event) : painting.handleStartDraw(event);
+});
+
+canvas.addEventListener('mousemove', event => {
+  if (painting.handleStartDraw) {
+    painting.handleMouseMove(event)
   }
-}
+});
+
+buttonClear.addEventListener('click', () => {
+  painting.redrawStoredLines();
+  painting.deleteAnimation();
+});
--- intersectionPoint.js
+++ intersectionPoint.js
@@ -1,74 +1,66 @@
 ///////////////////// ПОИСК ПЕРЕСЕЧЕНИЯ
-//векторное произведение
-function VEK(ax, ay, bx, by) {
-  return ax * by - bx * ay;
-}
 
-//проверка пересечения
-function CrossingCheck(p1, p2) {
-  let v1;
-  let v2;
-  let v3;
-  let v4;
+class Intersection {
+  constructor() {
+  };
 
-  v1 = VEK(p2.x2 - p2.x1, p2.y2 - p2.y1, p1.x1 - p2.x1, p1.y1 - p2.y1);
-  v2 = VEK(p2.x2 - p2.x1, p2.y2 - p2.y1, p1.x2 - p2.x1, p1.y2 - p2.y1);
-  v3 = VEK(p1.x2 - p1.x1, p1.y2 - p1.y1, p2.x1 - p1.x1, p2.y1 - p1.y1);
-  v4 = VEK(p1.x2 - p1.x1, p1.y2 - p1.y1, p2.x2 - p1.x1, p2.y2 - p1.y1);
+  VEK(ax, ay, bx, by) {
+    return ax * by - bx * ay;
+  };
 
-  if (v1 * v2 < 0 && v3 * v4 < 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
+  CrossingCheck(p1, p2) {
 
-//построение уравнения прямой Ax+By+C
-function EquationOfTheLine(p) {
-  A = p.y2 - p.y1;
-  B = p.x1 - p.x2;
-  C = -p.x1 * (p.y2 - p.y1) + p.y1 * (p.x2 - p.x1);
+    const v1 = this.VEK(p2.x2 - p2.x1, p2.y2 - p2.y1, p1.x1 - p2.x1, p1.y1 - p2.y1);
+    const v2 = this.VEK(p2.x2 - p2.x1, p2.y2 - p2.y1, p1.x2 - p2.x1, p1.y2 - p2.y1);
+    const v3 = this.VEK(p1.x2 - p1.x1, p1.y2 - p1.y1, p2.x1 - p1.x1, p2.y1 - p1.y1);
+    const v4 = this.VEK(p1.x2 - p1.x1, p1.y2 - p1.y1, p2.x2 - p1.x1, p2.y2 - p1.y1);
 
-}
+    if (v1 * v2 < 0 && v3 * v4 < 0) {
+      return true;
+    } else {
+      return false;
+    };
+  };
 
-// поиск точки пересечения по Х
-function IntersectionX(a1, b1, c1, a2, b2, c2) {
-  let d;
-  let dx;
-  let pointx;
-  d = a1 * b2 - b1 * a2;
-  dx = -c1 * b2 + b1 * c2;
-  pointx = dx / d;
-  return pointx;
-}
+  //построение уравнения прямой Ax+By+this.C
+  EquationOfTheLine(p) {
+    this.A = p.y2 - p.y1;
+    this.B = p.x1 - p.x2;
+    this.C = -p.x1 * (p.y2 - p.y1) + p.y1 * (p.x2 - p.x1);
+  };
 
-//поиск точки пересечения по Y
-function IntersectionY(a1, b1, c1, a2, b2, c2) {
-  let d;
-  let dy;
-  let pointy;
+  // поиск точки пересечения по Х
+  IntersectionX(a1, b1, c1, a2, b2, c2) {
+    const d = a1 * b2 - b1 * a2;
+    const dx = -c1 * b2 + b1 * c2;
+    const pointx = dx / d;
+    return pointx;
+  };
 
-  d = a1 * b2 - b1 * a2;
-  dy = -a1 * c2 + c1 * a2;
-  pointy = dy / d;
-  return pointy;
-}
+  //поиск точки пересечения по Y
+  IntersectionY(a1, b1, c1, a2, b2, c2) {
+    const d = a1 * b2 - b1 * a2;
+    const dy = -a1 * c2 + c1 * a2;
+    const pointy = dy / d;
+    return pointy;
+  };
 
-function intersectionPoint(firstLine, secondLine) {
-  ctx.fillStyle = "red";
-  if (CrossingCheck(firstLine, secondLine)) {
-    let a1;
-    let b1
-    let c1
-    let a2
-    let b2;
-    let c2;
-    EquationOfTheLine(firstLine);
-    a1 = A; b1 = B; c1 = C;
-    EquationOfTheLine(secondLine);
-    a2 = A; b2 = B; c2 = C;
-    pointxx = IntersectionX(a1, b1, c1, a2, b2, c2);
-    pointyy = IntersectionY(a1, b1, c1, a2, b2, c2);
-    ctx.fillRect(pointxx - 4, pointyy - 4, 8, 8);
-  }
-}
+  intersectionPoint(firstLine, secondLine ) {
+    ctx.fillStyle = "red";
+    if (this.CrossingCheck(firstLine, secondLine)) {
+      this.EquationOfTheLine(firstLine);
+      const a1 = this.A;
+      const b1 = this.B;
+      const c1 = this.C;
+      this.EquationOfTheLine(secondLine);
+      const a2 = this.A;
+      const b2 = this.B;
+      const c2 = this.C;
+      const pointxx = this.IntersectionX(a1, b1, c1, a2, b2, c2);
+      const pointyy = this.IntersectionY(a1, b1, c1, a2, b2, c2);
+      ctx.fillRect(pointxx - 4, pointyy - 4, 8, 8);
+    };
+  };
+};
+
+const intersection = new Intersection();
